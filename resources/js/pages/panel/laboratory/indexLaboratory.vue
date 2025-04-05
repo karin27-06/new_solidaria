df8b4b0c098abc82053157ec43f9290c0aa544bf
Karin Hair ChS <karinsntmr@gmail.com>
2025-04-05 10:44:30 -0500

    Styles placed in the laboratory section. (#7)

--- resources/js/pages/panel/laboratory/indexLaboratory.vue
+++ resources/js/pages/panel/laboratory/indexLaboratory.vue
@@ -1,7 +1,7 @@
 <template>
     <Head title="laboratory" />
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+        <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl p-2">
             <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
                 <FilterLaboratory @search="searchLaboratory" />
                 <TableLaboratory
