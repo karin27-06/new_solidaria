3a892a71de7a8f54b805eeadd333b4651861dcfd
junior15 <junior15062023@gmail.com>
2025-05-15 10:56:55 -0500

    char v1

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,9 @@
-import '../css/app.css';
-
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
+import '../css/app.css';
 import { initializeTheme } from './composables/useAppearance';
 
 // Extend ImportMeta interface for Vite...
@@ -13,7 +12,6 @@ declare module 'vite/client' {
         readonly VITE_APP_NAME: string;
         [key: string]: string | boolean | undefined;
     }
-
     interface ImportMeta {
         readonly env: ImportMetaEnv;
         readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
+import CustomerChart from '@/components/Charts/CustomerChart.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
-
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
         href: '/dashboard',
     },
 ];
+
+const columnsX = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
+const columnsY = [30, 40, 45, 50, 49, 60, 70];
 </script>
 
 <template>
@@ -18,18 +20,30 @@ const breadcrumbs: BreadcrumbItem[] = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="relative aspect-auto overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <CustomerChart :columns-x="columnsX" :columns-y="columnsY" title="USUARIOS" subtitle="Usuarios por local" chart-type="area" />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="relative aspect-auto overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <CustomerChart
+                        :columns-x="columnsX"
+                        :columns-y="columnsY"
+                        title="PRODUCTOS"
+                        subtitle="Productos por local"
+                        chart-type="scatter"
+                    />
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="relative aspect-auto overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                    <CustomerChart :columns-x="columnsX" :columns-y="columnsY" title="GUIAS" subtitle="guias por local" chart-type="bar" />
                 </div>
             </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <PlaceholderPattern />
+            <div class="relative h-[400px] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                <CustomerChart
+                    :columns-x="columnsX"
+                    :columns-y="columnsY"
+                    title="VENTAS"
+                    subtitle="Ventas por vendedor del local"
+                    chart-type="radar"
+                />
             </div>
         </div>
     </AppLayout>
